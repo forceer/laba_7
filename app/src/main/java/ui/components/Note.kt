922689ddd7eabe74813e198c3f9785174d628938
forceer <anton.klimchuk.2004@gmail.com>
2024-02-29 03:19:53 +0300

    background note

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -6,18 +6,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
-
+val backgroundShape: Shape = RoundedCornerShape(4.dp)
 @Composable fun Note() {
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+    ) {
         NoteColor(
             color = rwGreen,
             size = 40.dp,
