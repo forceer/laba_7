6d1fb961572730e0c36cea3b523f62b72828d0d4
forceer <anton.klimchuk.2004@gmail.com>
2024-03-01 23:11:39 +0300

    note prototype

--- app/src/main/java/ui/components/components/Note.kt
+++ app/src/main/java/ui/components/components/Note.kt
@@ -1,6 +1,7 @@
 package ui.components.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -37,6 +38,7 @@ val backgroundShape: Shape = RoundedCornerShape(4.dp)
             .fillMaxWidth()
             .heightIn(min = 64.dp)
             .background(Color.White, backgroundShape)
+            .clickable(onClick = { onNoteClick(note) })
     ) {
         NoteColor(
             modifier = Modifier
@@ -74,7 +76,10 @@ val backgroundShape: Shape = RoundedCornerShape(4.dp)
         if (note.isCheckedOff != null) {
             Checkbox(
                 checked = note.isCheckedOff,
-                onCheckedChange = {},
+                onCheckedChange = {isChecked ->
+                  val newNote = note.copy(isCheckedOff = isChecked)
+                  onNoteCheckedChange(newNote)
+                },
                 modifier = Modifier
                     .padding(start = 16.dp)
                     .align(Alignment.CenterVertically)
@@ -85,5 +90,12 @@ val backgroundShape: Shape = RoundedCornerShape(4.dp)
 @Preview
 @Composable
 private fun NotePreview(){
-    Note()
+    Note(
+        note = NoteModel(
+            1,
+            "Заметка 1",
+            "Содержимое 1",
+            null
+        )
+    )
 }
