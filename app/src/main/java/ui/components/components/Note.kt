1b9f56b8a7e414fd9080c39c6749edb142be0a81
forceer <anton.klimchuk.2004@gmail.com>
2024-03-10 17:46:59 +0300

    compose material for notes

--- app/src/main/java/ui/components/components/Note.kt
+++ app/src/main/java/ui/components/components/Note.kt
@@ -1,11 +1,7 @@
 package ui.components.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -15,20 +11,14 @@ import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.domain.model.NoteModel
 import com.topic2.android.notes.util.fromHex
 
-val backgroundShape: Shape = RoundedCornerShape(4.dp)
+
 @OptIn(ExperimentalMaterialApi::class)
 @Composable fun Note(
     modifier: Modifier = Modifier,
--- app/src/main/java/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/ui/components/screens/SaveNoteScreen.kt
@@ -1,14 +1,12 @@
 package ui.components.screens
 
 import android.annotation.SuppressLint
-import android.widget.CompoundButton.OnCheckedChangeListener
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
